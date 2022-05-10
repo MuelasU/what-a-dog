3c839b447f2535e86e3dd9bc56db786e1be63389
Gabriel Muelas <bimuelas@gmail.com>
2022-05-10 11:21:38 -0300

    Initial Commit

---
+++ what-a-dog/what_a_dogApp.swift
@@ -0,0 +1,17 @@
+//
+//  what_a_dogApp.swift
+//  what-a-dog
+//
+//  Created by Gabriel Muelas on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct what_a_dogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
