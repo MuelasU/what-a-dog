59a00e04d3069a3226e587e88158b6a1f5865884
Eduardo <eduardodiasdini@gmail.com>
2022-05-18 11:42:11 -0300

    Added models

---
+++ what-a-dog/Model/DogHeight.swift
@@ -0,0 +1,15 @@
+//
+//  DogHeight.swift
+//  what-a-dog
+//
+//  Created by Eduardo Dini on 18/05/22.
+//
+
+import Foundation
+
+struct DogHeight: Decodable {
+
+    var imperial: String
+    var metric: String
+    
+}
---
+++ what-a-dog/Model/DogWeight.swift
@@ -0,0 +1,15 @@
+//
+//  DogWeight.swift
+//  what-a-dog
+//
+//  Created by Eduardo Dini on 18/05/22.
+//
+
+import Foundation
+
+struct DogWeight: Decodable {
+
+    var imperial: String
+    var metric: String
+    
+}
