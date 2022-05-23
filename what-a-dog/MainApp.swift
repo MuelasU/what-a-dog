b686c077e50c6330e5bf8ed141714952a5fb98ba
Fusalina <zanotto.carol@gmail.com>
2022-05-23 09:02:44 -0300

    update: MainApp function from ModalView() to ContentView()

--- what-a-dog/MainApp.swift
+++ what-a-dog/MainApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct MainApp: App {
     var body: some Scene {
         WindowGroup {
-            ModalView()
-//            ContentView()
+            ContentView()
         }
     }
 }
