047f0b7d65f3bf9351d29180cb9ced5d00304305
Gabriel Muelas <bimuelas@gmail.com>
2022-05-24 09:27:51 -0300

    Fix merge conflicts

--- what-a-dog/Components/Cards/CardsView.swift
+++ what-a-dog/Components/Cards/CardsView.swift
@@ -105,15 +105,3 @@ struct Button_Previews: PreviewProvider {
         CardView()
     }
 }
-
-extension Image {
-    func centerCropped() -> some View {
-        GeometryReader { geo in
-            self
-                .resizable()
-                .scaledToFill()
-                .frame(width: geo.size.width, height: geo.size.height)
-                .clipped()
-        }
-    }
-}
