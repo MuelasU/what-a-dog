88f2671fb950a3302998fdaef318528bf4b4cf42
Eduardo <eduardodiasdini@gmail.com>
2022-05-25 09:47:10 -0300

    Fixed ListView bug

--- what-a-dog/Scenes/BreedsList/BreedsListView.swift
+++ what-a-dog/Scenes/BreedsList/BreedsListView.swift
@@ -11,19 +11,16 @@ import SwiftUI
 struct BreedsListView: View {
     @StateObject var viewModel = BreedListViewModel()
     var gridItemLayout = [GridItem(.flexible()), GridItem(.flexible())]
-    @State var selection: Int?
 
     var body: some View {
         ScrollView {
             LazyVGrid(columns: gridItemLayout, spacing: 8) {
                 ForEach(viewModel.dogs) { dog in
 
-                    NavigationLink(destination: CardView(breedName: dog.name), tag: 2, selection: $selection) {
+                    NavigationLink(destination: CardView(breedName: dog.name)) {
                         BreedsListViewCard(nome: dog.name, imageURL: URL(string: dog.image?.url ?? "")!)
-                            .onTapGesture {
-                                selection = 2
-                            }
                     }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
             .padding([.leading, .trailing])
