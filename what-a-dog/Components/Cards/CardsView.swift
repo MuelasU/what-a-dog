8bf97955eb19bd704e60b2fdd44f2ec374eb3578
fernando <fer20comp@gmail.com>
2022-05-26 09:12:07 -0300

    merge done

--- what-a-dog/Components/Cards/CardsView.swift
+++ what-a-dog/Components/Cards/CardsView.swift
@@ -114,18 +114,6 @@ struct Button_Previews: PreviewProvider {
     }
 }
 
-extension Image {
-    func centerCropped() -> some View {
-        GeometryReader { geo in
-            self
-                .resizable()
-                .scaledToFill()
-                .frame(width: geo.size.width, height: geo.size.height)
-                .clipped()
-        }
-    }
-}
-
 struct NavigationIndicator: UIViewControllerRepresentable {
     typealias UIViewControllerType = ARView
 
